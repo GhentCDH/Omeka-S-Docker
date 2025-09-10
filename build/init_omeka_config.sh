c58ffd0ed6185fadf0203497aad75196fb559226
Frederic Lamsens <frederic.lamsens@ugent.be>
2025-09-10 10:17:09 +0200

    chore: add env defaults

--- build/init_omeka_config.sh
+++ build/init_omeka_config.sh
@@ -4,10 +4,11 @@ cd /var/www/omeka-s
 
 # Create Omeka-S database.ini file
 echo "Creating Omeka S database.ini file ..."
-echo "user     = \"$MYSQL_USER\"" > /var/www/omeka-s/config/database.ini
-echo "password = \"$MYSQL_PASSWORD\"" >> /var/www/omeka-s/config/database.ini
-echo "dbname   = \"$MYSQL_DATABASE\"" >> /var/www/omeka-s/config/database.ini
-echo "host     = \"$MYSQL_HOST\"" >> /var/www/omeka-s/config/database.ini
+echo "user     = \"${MYSQL_USER:-omeka}\"" > /var/www/omeka-s/config/database.ini
+echo "password = \"${MYSQL_PASSWORD:-omeka}\"" >> /var/www/omeka-s/config/database.ini
+echo "dbname   = \"${MYSQL_DATABASE:-omeka}\"" >> /var/www/omeka-s/config/database.ini
+echo "host     = \"${MYSQL_HOST:-db}\"" >> /var/www/omeka-s/config/database.ini
+echo "port     = ${MYSQL_PORT:-3306}" >> /var/www/omeka-s/config/database.ini
 chmod 644 /var/www/omeka-s/config/database.ini
 
 # Update APPLICATION_ENV in .htaccess
