4bda182ad471e27f27d145f7f550b88b03a0b986
Frederic Lamsens <frederic.lamsens@ugent.be>
2025-09-26 17:33:37 +0200

    refactor: use omeka-s-cli to create database.ini

--- build/init_omeka_config.sh
+++ build/init_omeka_config.sh
@@ -3,12 +3,13 @@
 cd /var/www/omeka-s
 
 # Create Omeka-S database.ini file
-echo "Creating Omeka S database.ini file ..."
-echo "user     = \"${MYSQL_USER:-omeka}\"" > /var/www/omeka-s/config/database.ini
-echo "password = \"${MYSQL_PASSWORD:-omeka}\"" >> /var/www/omeka-s/config/database.ini
-echo "dbname   = \"${MYSQL_DATABASE:-omeka}\"" >> /var/www/omeka-s/config/database.ini
-echo "host     = \"${MYSQL_HOST:-db}\"" >> /var/www/omeka-s/config/database.ini
-echo "port     = ${MYSQL_PORT:-3306}" >> /var/www/omeka-s/config/database.ini
+# echo "Creating Omeka S database.ini file ..."
+omeka-s-cli config:create-db-ini --base-path /var/www/omeka-s \
+    --username "${MYSQL_USER:-omeka}" \
+    --password "${MYSQL_PASSWORD:-omeka}" \
+    --dbname "${MYSQL_DATABASE:-omeka}" \
+    --host "${MYSQL_HOST:-db}" \
+    --port "${MYSQL_PORT:-3306}"
 chmod 644 /var/www/omeka-s/config/database.ini
 
 # Update APPLICATION_ENV in .htaccess
